f9db6855b011cc8ce1d253a11aa1f73a5dc87369
TSentner <centner_dc@mail.ru>
2023-01-14 14:08:58 +0500

    Stumble anim play in ToRun & stumble anims

--- Assets/Scripts/Robber/CollisionImpact.cs
+++ Assets/Scripts/Robber/CollisionImpact.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 namespace Robber
@@ -5,10 +6,16 @@ namespace Robber
     [RequireComponent(typeof(Animator))]
     public class CollisionImpact : MonoBehaviour
     {
-        private readonly int _stumbleName = Animator.StringToHash("Stumble"),
-            _idleName = Animator.StringToHash("Idle"),
-            _runName = Animator.StringToHash("Run"),
-            _runWithBoxName = Animator.StringToHash("RunWithBox");
+        private readonly int _stumbleName = Animator.StringToHash("Stumble");
+        private readonly int[] _stumbleAnimationsReady =
+        {
+            Animator.StringToHash("Idle"),
+            Animator.StringToHash("Run"),
+            Animator.StringToHash("RunWithBox"),
+            Animator.StringToHash("GettingUp"),
+            Animator.StringToHash("StumbleBackwards"),
+            Animator.StringToHash("ToRoll"),
+        };
         
         [SerializeField] private float _maxVelocity = 15f;
         
@@ -26,8 +33,7 @@ namespace Robber
             {
                 var animationHash = _animator.
                     GetCurrentAnimatorStateInfo(0).shortNameHash;
-                if (animationHash == _runName || animationHash == _runWithBoxName
-                    || animationHash == _idleName)
+                if (_stumbleAnimationsReady.Contains(animationHash))
                 {
                     _animator.SetTrigger(_stumbleName);
                 }
